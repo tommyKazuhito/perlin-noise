13198146d013368bd8565d6f73f80c20f8484414
KazTom <kazuhito1oo1@gmail.com>
2022-10-18 15:18:23 +0900

    setup dev env

---
+++ src/script/counter.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+export function setupCounter(element: HTMLButtonElement) {
+  let counter = 0;
+  const setCounter = (count: number) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener('click', () => setCounter(++counter));
+  setCounter(0);
+}
